eb336bfe2e83e32ba563379f5760338a679910f1
minjae46 <minjaejo46@gmail.com>
2024-05-22 19:26:31 +0900

    Build UI

---
+++ app/create-account/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
+import FormInput from "@/components/form-input";
+import FormButton from "@/components/form-btn";
+import SocialLogin from "@/components/social-login";
+
+export default function CreateAccount() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput type="text" placeholder="Username" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="Confirm Password"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="Create account" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
+}
